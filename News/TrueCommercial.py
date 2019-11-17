229ae56a138957609da72e8922d4a09ddb09f938
Yhanli <45836848+Yhanli@users.noreply.github.com>
2019-11-18 12:05:22 +1300

    5

---
+++ News/TrueCommercial.py
@@ -0,0 +1,91 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*
+
+
+from conf import *
+from datetime import datetime, timedelta
+import copy, hashlib, oss2, json
+
+json_raw = {
+    "title": "",
+    "author": "",
+    "site_name": "",
+    "publish_date": "",
+    "cover": "",
+    "brief": "",
+    "news_category": "",
+    "comments": [],
+    "rawhtml": "",
+    "cleanhtml": ""
+}
+with open(currentDir + '/' + 'oss_auth', 'r') as Afile:
+    data = Afile.read().split('\n')
+auth = oss2.Auth(data[0], data[1])
+bucket = oss2.Bucket(auth, data[2], data[3], enable_crc=False)
+bucketSrc = 'https://s.hougarden.com/'
+bucketSrcEnd = '?x-oss-process=image/quality,q_80'
+
+next_page = False
+
+def get_news_body(url, json_data, count):
+    resp = GetUrlContent(url)
+    data = GetBSsoup(resp.text)
+    news_body = data.find('div', attrs={'class': 'content-container'})
+    json_data["author"] = news_body.find('span', class_="author").text if "True Commercial" not in news_body.find('span', class_="author").text else ""
+    json_data["site_name"] = "True Commercial"
+    json_data["title"] = news_body.find('h1').text
+    contents = news_body.find_all('p')
+    contents += news_body.find_all('div')
+    cover = upload_img_oss("http://truecommercial.oneroof.co.nz" + news_body.find('img').get("src"))
+    json_data["cover"] = cover[-1]
+    for p in contents:
+        if "Photo / Supplied" in str(p) or "feature-image" in str(p) or "span class" in str(p) or "<h1>" in str(p):
+            continue
+        json_data["cleanhtml"] = json_data["cleanhtml"] + str(p)
+
+    print(json_data["title"])
+    with open('extract/%s.json'%count, 'w+') as fp:
+        json.dump(json_data, fp, ensure_ascii=False)
+
+
+def upload_img_oss(img_link):
+    imgMD5 = str(hashlib.md5(img_link.encode('utf-8')).hexdigest())
+    bucketDir = 'article/' + \
+                imgMD5[:2] + '/' + imgMD5[2:4] + '/'
+    imgType = '.jpg'
+    fullDir = bucketDir + imgMD5 + imgType
+
+    response = requests.get(img_link.strip())
+    bucket.put_object(fullDir, response.content)
+    return [bucketSrc + fullDir + bucketSrcEnd, imgMD5 + imgType]
+
+while True:
+    link = "http://truecommercial.oneroof.co.nz/insights/news/?news=0&p=1&pp=799"
+
+    response = GetUrlContent(link)
+    testwrite(response.content)
+    data = GetBSsoup(response.content)
+
+    content_list = data.find_all('div', attrs={"class":"news-list-content"})
+    count = 1
+    for item in content_list:
+        try:
+            date = item.find("p", class_= "date").text.replace("Date ", "")
+            strTodate = datetime.strptime(date, "%b %Y")
+            if strTodate > datetime.now() - timedelta(days=190):
+                newslink = ("http://truecommercial.oneroof.co.nz" + item.find("a").get('href'))
+                json_item = copy.deepcopy(json_raw)
+                json_item["publish_date"] = strTodate.strftime("%Y-%m-%d")
+                json_item["news_category"] = "房地产"
+                json_item["brief"] = item.text.replace(item.find('h3').text,"").replace(item.find("p", class_= "date").text,"").replace("more","").strip()
+                get_news_body(url = newslink, json_data = json_item, count = str(count))
+                count += 1
+
+        except Exception as e:
+            print(str(e))
+            continue
+
+
+    if not next_page:
+        break
+
---
+++ News/conf.py
@@ -0,0 +1,241 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*
+import sqlite3, os, sys, logging, requests, random
+from bs4 import BeautifulSoup, re
+
+currentDir = os.path.dirname(os.path.realpath(__file__))
+try:
+    mode = sys.argv[1]
+except:
+    mode = "100"
+print("Mode %s" % mode)
+
+# DB = sqlite3.connect(currentDir + '/data.db', detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+header = [
+    {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36",
+        "Accept": "*/*",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
+        "origin": "https://wallstreetcn.com",
+        "Referer": "https://wallstreetcn.com/vip/articles/3476537",
+    },
+    {
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0.1 Safari/605.1.15"
+    },
+    {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0"
+    },
+    {"user-agent": "Opera/9.80 (Windows NT 6.1; WOW64) Presto/2.12.388 Version/12.18"},
+    {"user-agent": "Mozilla/5.0 (MSIE 10.0; Windows NT 6.1; Trident/5.0)"},
+]  # headers
+
+with open(
+    "/usr/local/services/spider/conf/proxies.txt", "r"
+) as proFile:  # currentDir + '/conf/proxies.txt'
+    proxies = [i.replace("\n", "").replace("\r", "") for i in proFile.readlines()]
+    proxies = [
+        "http://"
+        + i.split(":")[2]
+        + ":"
+        + i.split(":")[3]
+        + "@"
+        + i.split(":")[0]
+        + ":"
+        + i.split(":")[1]
+        for i in proxies
+    ]
+    proxies = [{"http": i, "https": i} for i in proxies]
+
+proxies = [proxies[1]]
+headers = [header[0]]
+
+with open("/usr/local/services/spider/conf/sendgrid_key", "r") as fp:
+    key = fp.read()
+
+
+def testwrite(data):
+    # return
+    try:
+        with open(currentDir + "/testwrite.html", "wb+") as fp:
+            fp.write(data)
+    except:
+        with open(currentDir + "/testwrite.html", "w+") as fp:
+            fp.write(data)
+
+
+def clear_db(DB, table):
+    try:
+        c = DB.cursor()
+        c.execute("DELETE FROM %s" % table)
+        DB.commit()
+    except Exception as e:
+        logger.debug("Failed to clear DB, %s" % str(e))
+
+
+def test_ip():
+    global proxies
+    proxy = random.choice(proxies)
+    header = random.choice(headers)
+    url = "https://whatismyipaddress.com/"
+    response = requests.get(url, headers=header, proxies=proxy)
+    checked = "IP == Proxy =========>>>{0} --------- {1}".format(
+        str(proxy).split("@")[-1].split(":")[0] in str(response.text),
+        str(proxy).split("@")[-1].split(":")[0],
+    )
+    logger.debug(checked)
+    return True if "True" in checked else False
+
+
+def GetUrlContent(url="", useProxy=True):
+    for i in range(2):
+        try:
+            if useProxy:
+                return requests.get(
+                    url,
+                    headers=random.choice(header),
+                    proxies=random.choice(proxies),
+                    timeout=10,
+                    allow_redirects=True,
+                )
+            else:
+                return requests.get(url, timeout=10, allow_redirects=True)
+            break
+        except Exception as e:
+            logger.debug(
+                "Error occur when requesting url: {url}:{error}".format(
+                    url=url, error=str(e)
+                )
+            )
+            if i >= 1:
+                return "Error occur when requesting url: {url}:{error}".format(
+                    url=url, error=str(e)
+                )
+
+
+def GetBSsoup(response_content):
+    return BeautifulSoup(response_content, features="lxml")
+
+
+def get_openTime(date_string):
+    ##### day
+    final_time_list = []
+    string_lower = date_string.lower()
+    final_time_list = []
+
+    if "7 day" in string_lower or "everyday" in string_lower or "7day" in string_lower:
+        for i in range(1,8):
+            final_time_list.append(
+                {"weekday": str(i), "start_time": "", "end_time": ""}
+            )
+            string_lower = string_lower.replace("7 day", "")
+            string_lower = string_lower.replace("7day", "")
+            string_lower = string_lower.replace("everyday", "")
+
+    elif "-" in string_lower or "to" in string_lower:
+        toggle = 0
+        start = ""
+        end = ""
+        weekday_list = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]
+        dic_weekday = {
+            "mon": 1,
+            "tue": 2,
+            "wed": 3,
+            "thu": 4,
+            "fri": 5,
+            "sat": 6,
+            "sun": 7,
+        }
+        for a in re.sub("[^a-z0-9.:]", " ", string_lower).split(" "):
+            for i in weekday_list:
+                if i in a:
+                    if toggle == 0:
+                        start = dic_weekday[i]
+                        toggle = 1
+                        string_lower = string_lower.replace(i, "")
+                    else:
+                        end = dic_weekday[i]
+                        string_lower = string_lower.replace(i, "")
+
+        if start < end:
+            for i in range(start, end + 1):
+                final_time_list.append({"weekday": i, "start_time": "", "end_time": ""})
+        elif end < start:
+            for i in range(start, 8):
+                final_time_list.append({"weekday": i, "start_time": "", "end_time": ""})
+            for i in range(1, end + 1):
+                final_time_list.append({"weekday": i, "start_time": "", "end_time": ""})
+
+    else:
+        if "mon" in re.sub("[^a-z0-9]", "", string_lower):
+            final_time_list.append({"weekday": 1, "start_time": "", "end_time": ""})
+        if "tue" in re.sub("[^a-z0-9]", "", string_lower):
+            final_time_list.append({"weekday": 2, "start_time": "", "end_time": ""})
+        if "wed" in re.sub("[^a-z0-9]", "", string_lower):
+            final_time_list.append({"weekday": 3, "start_time": "", "end_time": ""})
+        if "thu" in re.sub("[^a-z0-9]", "", string_lower):
+            final_time_list.append({"weekday": 4, "start_time": "", "end_time": ""})
+        if "fri" in re.sub("[^a-z0-9]", "", string_lower):
+            final_time_list.append({"weekday": 5, "start_time": "", "end_time": ""})
+        if "sat" in re.sub("[^a-z0-9]", "", string_lower):
+            final_time_list.append({"weekday": 6, "start_time": "", "end_time": ""})
+        if "sun" in re.sub("[^a-z0-9]", "", string_lower):
+            final_time_list.append({"weekday": 7, "start_time": "", "end_time": ""})
+
+    ###### time
+    date_string = string_lower.replace(":00", "-").replace(".00", "-")
+    time_list = [s for s in re.sub("[^0-9:.]", "-", date_string).split("-") if s != ""]
+    start_time = 0
+    end_time = 0
+    toggle = 0
+
+    for i in time_list:
+        try:
+            i = int(i)
+        except:
+            continue
+        if i > 7 and toggle == 0:
+            start_time = i
+            toggle = 1
+        if i < 4 and toggle == 0:
+            start_time = i + 12
+            toggle = 1
+        elif i < 12 and toggle != 0 and i < start_time:
+            end_time = i + 12
+        elif i <= 12 and toggle != 0:
+            end_time = i
+
+    if start_time > end_time:
+        tmp = end_time
+        end_time = start_time
+        start_time = tmp
+
+    if start_time < 12:
+        start_time = str(start_time) + "am"
+    else:
+        if start_time == 12:
+            start_time = str(start_time) + "pm"
+        else:
+            start_time = str(start_time - 12) + "pm"
+    if end_time < 12:
+        end_time = str(end_time) + "am"
+    else:
+        if end_time == 12:
+            end_time = str(end_time) + "pm"
+        else:
+            end_time = str(end_time - 12) + "pm"
+
+    return_time = []
+    for i in final_time_list:
+        i["start_time"] = start_time
+        i["end_time"] = end_time
+        return_time.append(i)
+
+    return return_time
+
+
+# will not need to set up currentDir, data.db will always be generated with types enable, loaded proxies, common headers
+
